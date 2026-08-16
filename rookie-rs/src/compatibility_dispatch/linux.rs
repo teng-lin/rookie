f750e4befcf1df67daee6c663f4fb7e2cd18ab88
Teng Lin <teng.lin@gmail.com>
2026-08-15 21:35:05 -0400

    Contain compatibility dispatch by target

---
+++ rookie-rs/src/compatibility_dispatch/linux.rs
@@ -0,0 +1,82 @@
+use std::path::PathBuf;
+
+use anyhow::bail;
+
+use super::LoadFn;
+use crate::{Cookie, Result};
+
+pub(super) fn extend_legacy_load_browsers(browser_types: &mut Vec<(&'static str, LoadFn)>) {
+  browser_types.push(("chrome", crate::chrome));
+  browser_types.push(("cachy", crate::cachy));
+}
+
+pub(super) fn opera_gx(domains: Option<Vec<String>>) -> Result<Vec<Cookie>> {
+  let _ = domains;
+  bail!("Opera GX is not supported on Linux")
+}
+
+pub(super) fn chromium_from_path(
+  cookies_path: PathBuf,
+  domains: Option<Vec<String>>,
+  _key_path: Option<&str>,
+) -> Result<Vec<Cookie>> {
+  chromium_from_path_with(
+    cookies_path,
+    domains,
+    crate::direct_path::legacy_automatic_chromium,
+  )
+}
+
+fn chromium_from_path_with<F>(
+  cookies_path: PathBuf,
+  domains: Option<Vec<String>>,
+  query: F,
+) -> Result<Vec<Cookie>>
+where
+  F: FnOnce(PathBuf, Option<Vec<String>>) -> Result<Vec<Cookie>>,
+{
+  query(cookies_path, domains)
+}
+
+pub(super) fn safari_from_path(
+  _cookies_path: PathBuf,
+  _domains: Option<Vec<String>>,
+) -> Result<Vec<Cookie>> {
+  bail!("Safari binary cookie files are only supported on macOS")
+}
+
+pub(super) fn internet_explorer_from_path(
+  _cookies_path: PathBuf,
+  _domains: Option<Vec<String>>,
+) -> Result<Vec<Cookie>> {
+  bail!("Internet Explorer WebCache files are only supported on Windows")
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn chromium_bridge_forwards_owned_path_and_domains() {
+    let path = PathBuf::from("/tmp/Cookies");
+    let domains = Some(vec!["example.test".to_owned()]);
+    chromium_from_path_with(
+      path.clone(),
+      domains.clone(),
+      |actual_path, actual_domains| {
+        assert_eq!(actual_path, path);
+        assert_eq!(actual_domains, domains);
+        Ok(Vec::new())
+      },
+    )
+    .expect("injected Chromium query");
+  }
+
+  #[test]
+  fn opera_gx_keeps_exact_linux_error() {
+    assert_eq!(
+      opera_gx(None).unwrap_err().to_string(),
+      "Opera GX is not supported on Linux"
+    );
+  }
+}
